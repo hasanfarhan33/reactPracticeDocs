202bc391e176e49fac7737580873880e47d43155
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-19 13:35:21 +0300

    Learnt about variables and functions

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, Image, ScrollView, TextInput } from "react-native";
 
 const YourApp = () => {
+  const myName = "Goomba";
+  const getFullName = (firstName, secondName) => {
+    return firstName + " " + secondName;
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Try editing me! 🎉</Text>
-    </View>
+    <ScrollView>
+      <Text>This text is inside scroll view</Text>
+      <View>
+        <Text>My name is {myName}.</Text>
+        <Text>My name is {getFullName("Farhan", "Hasan")}</Text>
+        <Image
+          source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+          style={{ width: 150, height: 150 }}
+        ></Image>
+      </View>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        defaultValue="You can type whatever the hell you wanna type here"
+      ></TextInput>
+    </ScrollView>
   );
 };
 
